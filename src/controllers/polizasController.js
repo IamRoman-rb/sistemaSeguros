59d9095db6d3ea3de1b4d6f09d9ff6baebb96f4f
IamRoman-rb <roman.borla2004@gmail.com>
2025-01-02 17:13:13 -0300

    Mostrando datos de polizas.json en la vista polizas.ejs

--- src/controllers/polizasController.js
+++ src/controllers/polizasController.js
@@ -1,10 +1,23 @@
+import path from "path";
+import fs from "fs";
 const polizasController = {
-    index: (req, res) => {
-      res.render('polizas/polizas'); // Renderiza la vista 'polizas.ejs'
+    index: async (req, res) => {
+      try {
+        const polizasPath = path.resolve(process.cwd(), "src/data", "polizas.json");
+        console.log(polizasPath);
+        
+        const polizasData = await fs.promises.readFile(polizasPath, 'utf8');
+        const polizas = JSON.parse(polizasData);
+  
+        res.render('polizas/polizas', { polizas });
+      } catch (error) {
+        console.error('Error al cargar las pólizas:', error.message);
+        res.status(500).send('Error al cargar las pólizas: ' + error.message);
+      }
     },
     nueva: (req, res) => {
       res.render('polizas/nueva');
-    }
+    },
   };
   
   export default polizasController;
