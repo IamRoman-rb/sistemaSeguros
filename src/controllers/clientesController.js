5ff694149ff41cecb004ad73a93035185b9c7fd0
IamRoman-rb <roman.borla2004@gmail.com>
2025-01-03 21:18:48 -0300

    Boton para eliminar cliente, vista de confirmacion y eliminar sus polizas asociadas

--- src/controllers/clientesController.js
+++ src/controllers/clientesController.js
@@ -86,7 +86,42 @@ const clientesController = {
         console.error('Error al crear el cliente:', error.message);
         res.status(500).send('Error al crear el cliente');
     }
-    }
+    },
+    confirmar: async (req, res) => {
+        const { id } = req.params;
+
+        res.render('alertas/eliminarCliente', { clienteId: id });
+    },
+    eliminar: async (req, res) =>{
+      try {
+      const { id } = req.params;
+
+      // Ruta al archivo JSON de clientes
+      const clientesPath = path.resolve(process.cwd(), "src/data", "clientes.json");
+      // Ruta al archivo JSON de pólizas
+      const polizasPath = path.resolve(process.cwd(), "src/data", "polizas.json");
+
+      // Leer los datos de los archivos JSON
+      const clientesData = await fs.promises.readFile(clientesPath, 'utf8');
+      const clientes = JSON.parse(clientesData);
+      const polizasData = await fs.promises.readFile(polizasPath, 'utf8');
+      const polizas = JSON.parse(polizasData);
+
+      // Filtrar los clientes para eliminar el que coincide con el ID
+      const clientesFiltrados = clientes.filter(cliente => cliente.id !== Number(id));
+
+      // Filtrar las pólizas que pertenecen al cliente a eliminar
+      const polizasFiltradas = polizas.filter(poliza => !clientesFiltrados.some(cliente => cliente.polizas.includes(poliza.id)));
+
+      // Escribir los clientes y pólizas filtrados en sus respectivos archivos JSON
+      await fs.promises.writeFile(clientesPath, JSON.stringify(clientesFiltrados, null, 2));
+      await fs.promises.writeFile(polizasPath, JSON.stringify(polizasFiltradas, null, 2));
+
+      res.redirect('/clientes');
+  } catch (error) {
+      console.error('Error al eliminar el cliente:', error.message);
+      res.status(500).send('Error al eliminar el cliente');
+  }}
   };
   
   export default clientesController;
--- src/routes/clientes.js
+++ src/routes/clientes.js
@@ -5,7 +5,9 @@ const router = Router();
 
 router.get("/clientes", clientes.index);
 router.get("/clientes/nuevo", clientes.nuevo);
-router.post('/clientes/nuevo', clientes.crearCliente)
+router.post('/clientes/nuevo', clientes.crearCliente);
 router.get('/clientes/detalle/:id', clientes.detalle);
+router.get('/clientes/confirmar/:id', clientes.confirmar);
+router.get('/clientes/eliminar/:id', clientes.eliminar);
 
 export default router;
