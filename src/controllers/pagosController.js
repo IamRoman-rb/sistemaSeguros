d910f6909f373936ccba5e07340133e9fa295196
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-01-09 18:10:31 -0300

    Ajuste de pagos

--- src/controllers/pagosController.js
+++ src/controllers/pagosController.js
@@ -1,152 +1,146 @@
 import path from 'path';
-import fs from 'fs';
+import {readFile,writeFile} from 'node:fs/promises';
 
-const pagosController = {
-  index: async (req, res) => {
-    const pagosPath = path.resolve(process.cwd(), "src/data", "pagos.json");
-    const clientesPath = path.resolve(process.cwd(), "src/data", "clientes.json");
-    const polizasPath = path.resolve(process.cwd(), "src/data", "polizas.json");
-    const usuariosPath = path.resolve(process.cwd(), "src/data", "usuarios.json");
+export const listar = async (req, res) => {
+
+  const resources = [
+    path.resolve(process.cwd(), "src/data", "pagos.json"),
+    path.resolve(process.cwd(), "src/data", "clientes.json"),
+    path.resolve(process.cwd(), "src/data", "polizas.json"),
+    path.resolve(process.cwd(), "src/data", "usuarios.json"),
+  ];
+
+  try {
+    const [pagos, clientes, polizas, usuarios]= await Promise.all(resources.map(async (file) => JSON.parse(await readFile(file, 'utf-8'))));
+
+    let pagosConDetalles = pagos.map(pago => ({
+      ...pago,
+      cliente: clientes.find(c => c.id === pago.id_cliente) || { nombre: 'Cliente no encontrado' },
+      poliza: polizas.find(p => p.id === pago.id_poliza) || { n_poliza: 'Póliza no encontrada' },
+      cobrador: usuarios.find(u => u.id === parseInt(pago.id_cobrador)) || { nombre: 'Cobrador no encontrado' },
+    }));
+
+
+    if(["Empleado"].includes(req.session.user.rol)) {
+      pagosConDetalles = pagosConDetalles.filter(pago => pago.cobrador.id === req.session.user.id);
+    }
+
+
+    res.render('pagos/pagos', { pagos: pagosConDetalles });
+  } catch (error) {
+    console.error('Error al procesar los datos:', error);
+    res.status(500).send('Error interno del servidor'); // Handle unexpected errors gracefully
+  }
+};
+
+  export const detalle = async (req, res) => {
+    const { id } = req.params;
+  
+    const resources = [
+      path.resolve(process.cwd(), "src/data", "pagos.json"),
+      path.resolve(process.cwd(), "src/data", "clientes.json"),
+      path.resolve(process.cwd(), "src/data", "polizas.json"),
+      path.resolve(process.cwd(), "src/data", "usuarios.json"),
+    ];
   
     try {
-      // Read and parse the payments data
-      const pagosData = await fs.promises.readFile(pagosPath, "utf8");
-      const clientesData = await fs.promises.readFile(clientesPath, 'utf8');
-      const polizasData = await fs.promises.readFile(polizasPath, 'utf8');
-      const usuariosData = await fs.promises.readFile(usuariosPath, 'utf8');
+      const [pagosData, clientesData, polizasData, usuariosData] = await Promise.all(resources.map(readFile));
   
-      // Parse the data (handle potential parsing errors)
-      let pagos;
-      let clientes;
-      let polizas;
-      let usuarios;
-      try {
-        pagos = JSON.parse(pagosData);
-        clientes = JSON.parse(clientesData);
-        polizas = JSON.parse(polizasData);
-        usuarios = JSON.parse(usuariosData);
-      } catch (parseError) {
-        console.error('Error al parsear los datos:', parseError);
-        res.status(500).send('Error interno del servidor (datos corruptos)');
-        return; // Exit the function if parsing fails
+      const [pagos, clientes, polizas, usuarios] = await Promise.all([
+        pagosData.then(JSON.parse),
+        clientesData.then(JSON.parse),
+        polizasData.then(JSON.parse),
+        usuariosData.then(JSON.parse),
+      ]);
+  
+      const pagosEnriquecidos = pagos.map(pago => ({
+        ...pago,
+        cliente: clientes.find(c => c.id === pago.id_cliente) || { nombre: 'Cliente no encontrado' },
+        poliza: polizas.find(p => p.id === pago.id_poliza) || { n_poliza: 'Póliza no encontrada' },
+        cobrador: usuarios.find(u => u.id === parseInt(pago.id_cobrador)) || { nombre: 'Cobrador no encontrado' },
+      }));
+  
+      const pago = pagosEnriquecidos.find(p => p.id === parseInt(id));
+  
+      if (!pago) {
+        return res.status(404).send('Pago no encontrado');
       }
   
-      // Map payments to enriched objects with details
-      const pagosConDetalles = pagos.map(async (pago) => {
-        const [cliente, poliza, cobrador] = await Promise.all([
-          clientes.find(c => c.id === pago.id_cliente),
-          polizas.find(p => p.id === pago.id_poliza),
-          usuarios.find(u => u.id === parseInt(pago.id_cobrador)),
-        ]);
-  
-        // Handle cases where details might not be found
-        return {
-          ...pago,
-          cliente: cliente || { nombre: 'Cliente no encontrado' }, // Default value for missing client
-          poliza: poliza || { n_poliza: 'Póliza no encontrada' }, // Default value for missing policy
-          cobrador: cobrador || { nombre: 'Cobrador no encontrado' }, // Default value for missing cobrador
-        };
-      });
-       
-
-      // Wait for all promises to resolve before rendering
-      const pagosEnriquecidos = await Promise.all(pagosConDetalles);
-  
-      res.render('pagos/pagos', { pagos: pagosEnriquecidos });
+      res.render('pagos/detalle', { pago });
     } catch (error) {
       console.error('Error al procesar los datos:', error);
       res.status(500).send('Error interno del servidor'); // Handle unexpected errors gracefully
     }
-  },
-  nuevo: async (req, res) => {
-        try {
-            const { busqueda } = req.query || {};
-    
-            const clientesPath = path.resolve(process.cwd(), "src/data", "clientes.json");
-            const polizasPath = path.resolve(process.cwd(), "src/data", "polizas.json");
-            
-            // Leer los datos de clientes
-            const clientesData = await fs.promises.readFile(clientesPath, "utf8");
-            const clientes = JSON.parse(clientesData);
-    
-            // Leer los datos de pólizas
-            const polizasData = await fs.promises.readFile(polizasPath, "utf8");
-            const polizas = JSON.parse(polizasData);
-    
-            // Añadir el cliente correspondiente a cada póliza
-            const polizasConClientes = polizas.map((poliza) => {
-                const cliente = clientes.find((c) => c.polizas.includes(poliza.id));
-                return {
-                    ...poliza,
-                    cliente,
-                };
-            });
-    
-            // Filtrar las pólizas si hay un término de búsqueda
-            let polizasFiltradas = polizasConClientes;
-            if (busqueda) {
-                const busquedaLower = busqueda.toLowerCase();
-                polizasFiltradas = polizasConClientes.filter((poliza) => {
-                    return (
-                        (poliza.cliente && poliza.cliente.nombre.toLowerCase().includes(busquedaLower)) ||
-                        (poliza.n_poliza && poliza.n_poliza.toString().includes(busqueda)) ||
-                        (poliza.patente && poliza.patente.toLowerCase().includes(busquedaLower))
-                    );
-                });
-            }
-    
-            // Renderizar la vista con las pólizas filtradas y la búsqueda (si existe)
-            res.render("pagos/nuevo", { polizas: polizasFiltradas, busqueda });
-        } catch (error) {
-            console.error("Error al cargar las pólizas:", error.message);
-            res.status(500).send("Error al cargar las pólizas: " + error.message);
-        }
-  },
-  pagarPoliza: async (req, res) => {
-    const { id } = req.params;
-    const polizasPath = path.resolve(process.cwd(), "src/data", "polizas.json");
-    const clientesPath = path.resolve(process.cwd(), "src/data", "clientes.json");
+  };
 
-    // Leer los datos de los archivos
-    const polizasData = await fs.promises.readFile(polizasPath, 'utf-8');
-    const clientesData = await fs.promises.readFile(clientesPath, 'utf-8');
-    const polizas = JSON.parse(polizasData);
-    const clientes = JSON.parse(clientesData);
+  export const nuevo = async (req, res) => {
+    const {parametro=null, busqueda=null} = req.query;
+    const resources = [
+     path.resolve(process.cwd(), "src/data", "clientes.json"),
+     path.resolve(process.cwd(), "src/data", "polizas.json"),
+    ];
+    const [clientes, polizas]= await Promise.all(resources.map(async (file) => JSON.parse(await readFile(file, 'utf-8'))));
+
+    const polizasConClientes = polizas.map((poliza) => ({
+      ...poliza,
+      cliente: clientes.find((c) => c.polizas.includes(poliza.id)),
+    }));
+
+    const hoy = new Date();
+
+    if( parametro && busqueda){
+      const polizasFiltradas = polizasConClientes.filter((poliza) => {
+        const busquedaLower = busqueda.toLowerCase();
+        switch (parametro) {
+          case "n_poliza":
+            return poliza.n_poliza && poliza.n_poliza.toString().includes(busqueda);
+          case "patente":
+            return poliza.patente && poliza.patente.toLowerCase().includes(busquedaLower);
+          case "cliente":
+            return poliza.cliente && poliza.cliente.nombre.toLowerCase().includes(busquedaLower);
+          default:
+            return false;
+        }
+      })
+     return res.render("pagos/nuevo", { polizas: polizasFiltradas, busqueda, parametro });
+    }
+    const polizasFiltradas = polizasConClientes.filter((poliza) => {
+      const fechaVigencia = new Date(poliza.f_fin_vigencia);
+      const diferencia = fechaVigencia.getTime() - hoy.getTime();
+      return diferencia > 0 && diferencia < 31 * 24 * 60 * 60 * 1000;
+    });
+
+    //return res.status(200).json({ polizas: polizasFiltradas, busqueda, parametro });
+    return res.render("pagos/nuevo", { polizas: polizasFiltradas, busqueda, parametro });
+  };
+
+  export const pagar = async (req, res) => {
+    const { id } = req.params;
+    const resources = [
+      path.resolve(process.cwd(), "src/data", "polizas.json"),
+      path.resolve(process.cwd(), "src/data", "clientes.json"),
+    ];
+    const [polizas, clientes] = await Promise.all(resources.map(async (file) => JSON.parse(await readFile(file, 'utf-8'))));
 
     // Encontrar la póliza por ID
     const poliza = polizas.find(poliza => poliza.id === parseInt(id));
-
-    if (!poliza) {
-        return res.status(404).send('Póliza no encontrada');
-    }
+    if (!poliza) return res.status(404).send('Póliza no encontrada');
 
     // Encontrar el cliente correspondiente a la póliza
     const cliente = clientes.find(cliente => cliente.id === parseInt(poliza.clienteId));
 
     // Enviar la póliza y el cliente a la vista
     res.render('pagos/pagarPoliza', { poliza, cliente });
-},
-realizarPago: async (req, res) => {
+  };
+export const acreditar = async (req, res) => {
   try {
-    const id = req.body.polizaId;
+    const {polizaId:id} = req.body;
 
-    const polizasPath = path.resolve(process.cwd(), "src/data", "polizas.json");
-    const pagosPath = path.resolve(process.cwd(), "src/data", "pagos.json");
-
-    // Obtener el usuario autenticado desde la sesión o el sistema de autenticación
-    const usuarioAutenticado = req.user; // Asegúrate de que `req.user` contiene la información del usuario
-    if (!usuarioAutenticado) {
-      return res.status(401).send("Usuario no autenticado.");
-    }
-
-    // Leer los datos de los archivos
-    const [polizasData, pagosData] = await Promise.all([
-      fs.promises.readFile(polizasPath, "utf-8"),
-      fs.promises.readFile(pagosPath, "utf-8"),
-    ]);
-
-    const polizas = JSON.parse(polizasData);
-    const pagos = JSON.parse(pagosData);
+    const resources = [
+      path.resolve(process.cwd(), "src/data", "polizas.json"),
+      path.resolve(process.cwd(), "src/data", "pagos.json"),
+    ];
+    const [polizas, pagos] = await Promise.all(resources.map(async (file) => JSON.parse(await readFile(file, 'utf-8'))));
 
     // Encontrar la póliza
     const poliza = polizas.find((p) => p.id === parseInt(id));
@@ -168,16 +162,16 @@ realizarPago: async (req, res) => {
       hora: `${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`, // Hora actual
       monto: poliza.precio, // Precio de la póliza
       cuota: (poliza.cuotasPagas || 0) + 1, // Incrementar cuotas pagadas
-      id_cobrador: usuarioAutenticado.id, // ID del usuario autenticado
+      id_cobrador: req.session.user.id, // ID del usuario autenticado
     };
 
     // Actualizar el archivo de pagos
     pagos.push(pago);
-    await fs.promises.writeFile(pagosPath, JSON.stringify(pagos, null, 2));
+    await writeFile(resources[1], JSON.stringify(pagos, null, 2));
 
     // Actualizar la póliza con las cuotas pagadas
     poliza.cuotasPagas = pago.cuota;
-    await fs.promises.writeFile(polizasPath, JSON.stringify(polizas, null, 2));
+    await writeFile(resources[0], JSON.stringify(polizas, null, 2));
 
     // Redirigir a la lista de pagos o a una vista de confirmación
     res.redirect("/pagos");
@@ -186,7 +180,3 @@ realizarPago: async (req, res) => {
     res.status(500).send("Error al realizar el pago.");
   }
 }
-
-};
-  
-  export default pagosController;
--- src/routes/pagos.js
+++ src/routes/pagos.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import pagos from "../controllers/pagosController.js"; // Usamos 'import' en lugar de 'require'
+import {listar, nuevo,pagar,acreditar} from "../controllers/pagosController.js"; // Usamos 'import' en lugar de 'require'
 import hasPermitions from "../middlewares/hasPermitions.js";
 
 const router = Router();
 
-router.get("/", hasPermitions, pagos.index);
-router.get('/nuevo', hasPermitions, pagos.nuevo);
+router.get("/", hasPermitions, listar);
+router.get('/nuevo', hasPermitions, nuevo);
 // router.get('/detalle/:id', hasPermitions, pagos.detalle); // TODO: HACER
-router.get('/pagar/:id', hasPermitions, pagos.pagarPoliza);
-router.post('/acreditar', pagos.realizarPago);
+router.get('/pagar/:id', hasPermitions, pagar);
+router.post('/acreditar', acreditar);
 // router.post('/guardar', pagos.crearPago); // TODO: HACER
 // router.post('/actualizar', pagos.actualizarPago); // TODO: HACER
 
