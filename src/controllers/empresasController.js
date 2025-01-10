9108f82c5b93d95f34994b41effbe44862c37d03
IamRoman-rb <roman.borla2004@gmail.com>
2025-01-10 17:09:36 -0300

    Formulario para crear empresa

--- src/controllers/empresasController.js
+++ src/controllers/empresasController.js
@@ -1,215 +1,219 @@
 import path from "path";
 import fs from "fs";
-import bcrypt from "bcrypt";
-import e from "express";
-
 
 export const listar = async (req, res) => {
-    try {
-      const empresasPath = path.resolve(
-        process.cwd(),
-        "src/data",
-        "empresas.json"
-      );
-      const coberturasPath = path.resolve(
-        process.cwd(),
-        "src/data",
-        "coberturas.json"
-      );
-      const empresasData = await fs.promises.readFile(empresasPath, "utf8");
-      const empresas = JSON.parse(empresasData);
+  try {
+    const empresasPath = path.resolve(
+      process.cwd(),
+      "src/data",
+      "empresas.json"
+    );
+    const coberturasPath = path.resolve(
+      process.cwd(),
+      "src/data",
+      "coberturas.json"
+    );
+    const empresasData = await fs.promises.readFile(empresasPath, "utf8");
+    const empresas = JSON.parse(empresasData);
 
-      const coberturasData = await fs.promises.readFile(coberturasPath, "utf-8");
-      const coberturas = JSON.parse(coberturasData);
+    const coberturasData = await fs.promises.readFile(coberturasPath, "utf-8");
+    const coberturas = JSON.parse(coberturasData);
 
+    res.render("empresas/empresas", { empresas, coberturas });
+  } catch (error) {
+    console.error("Error al cargar las empresas:", error.message);
+    res.status(500).send("Error al cargar las empresas");
+  }
+};
+export const nuevo = async (req, res) => {
+  const coberturasPath = path.resolve(
+    process.cwd(),
+    "src/data",
+    "coberturas.json"
+  );
 
-      res.render("empresas/empresas", { empresas, coberturas });
-    } catch (error) {
-      console.error("Error al cargar las empresas:", error.message);
-      res.status(500).send("Error al cargar las empresas");
-    }
-  };
-export const  nuevo = async (req, res) => {
-    res.render("empresas/nueva");
-  };
+  const coberturasData = await fs.promises.readFile(coberturasPath, "utf-8");
+  const coberturas = JSON.parse(coberturasData);
+
+  res.render("empresas/nueva", { coberturas });
+};
 
 export const guardar = async (req, res) => {
-    try {
-      const { nombre } = req.body;
+  try {
+    const { nombre, coberturas } = req.body;
 
-      if (!nombre) {
-        return res.status(400).send("El campo nombre es obligatorio");
-      }
-
-      const empresasPath = path.resolve(
-        process.cwd(),
-        "src/data",
-        "empresas.json"
-      );
-      const empresasData = await fs.promises.readFile(empresasPath, "utf8");
-      const empresas = JSON.parse(empresasData);
-
-      // Verificar si el usuario ya existe por DNI
-      const empresaExistente = empresas.find((e) => e.nombre === nombre);
-      if (empresaExistente) {
-        return res.status(400).send("El usuario con ese DNI ya existe");
-      }
-
-      // Generar un nuevo ID
-      const nuevoId = empresas.length + 1;
-
-      const nuevaEmpresa = {
-        id: nuevoId,
-        nombre,
-        coberturas: []
-      };
-
-      empresas.push(nuevaEmpresa);
-
-      // Guardar los cambios en el archivo JSON
-      fs.writeFileSync(empresasPath, JSON.stringify(empresas, null, 2));
-
-      res.redirect("/empresas");
-    } catch (error) {
-      console.error("Error al crear una nueva empresa:", error.message);
-      res.status(500).send("Error al crear una nueva empresa");
+    if (!nombre) {
+      return res.status(400).send("El campo nombre es obligatorio");
     }
-  };
 
-
-export const detalle =  async (req, res) => {
-    try {
-      const { id } = req.params; // Obtener el ID del usuario desde la URL
-
-      const usuariosPath = path.resolve(
-        process.cwd(),
-        "src/data",
-        "usuarios.json"
-      );
-      const permisosPath = path.resolve(
-        process.cwd(),
-        "src/data",
-        "permisos.json"
-      );
-      const sucursalesPath = path.resolve(
-        process.cwd(),
-        "src/data",
-        "sucursales.json"
-      );
-      const usuariosData = await fs.promises.readFile(usuariosPath, "utf8");
-      const permisosData = await fs.promises.readFile(permisosPath, "utf8");
-      const sucursalesData = await fs.promises.readFile(sucursalesPath, "utf8");
-      const usuarios = JSON.parse(usuariosData);
-      const permisos = JSON.parse(permisosData);
-      const sucursales = JSON.parse(sucursalesData);
-
-      const usuario = usuarios.find((u) => u.id === parseInt(id));
-
-      if (!usuario) {
-        return res.status(404).send("Usuario no encontrado");
-      }
-
-      const permiso = permisos.find((p) => p.id === usuario.permisos);
-      const sucursal = sucursales.find((s) => s.id === usuario.sucursal);
-
-      res.render("usuarios/detalle", { usuario, permiso, sucursal });
-    } catch (error) {
-      console.error("Error al cargar el detalle del usuario:", error.message);
-      res.status(500).send("Error al cargar el detalle del usuario");
-    }
-  };
-
-  // Método para mostrar el formulario de edición
-export const editar = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const empresasPath = path.resolve(
-        process.cwd(),
-        "src/data",
-        "empresas.json"
-      );
-      
-      const empresasData = await fs.promises.readFile(empresasPath, "utf8");
-      const empresas = JSON.parse(empresasData);
-
-      const empresa = empresas.find((e) => e.id === parseInt(id));    
+    const empresasPath = path.resolve(
+      process.cwd(),
+      "src/data",
+      "empresas.json"
+    );
     
-      res.render("empresas/editar", { empresa });
-    } catch (error) {
-      console.error("Error al cargar el formulario de edición:", error.message);
-      res.status(500).send("Error al cargar el formulario de edición");
+    const empresasData = await fs.promises.readFile(empresasPath, "utf8");
+    const empresas = JSON.parse(empresasData);
+    
+    // Verificar si el usuario ya existe por DNI
+    const empresaExistente = empresas.find((e) => e.nombre === nombre);
+    if (empresaExistente) {
+      return res.status(400).send("El usuario con ese DNI ya existe");
     }
-  };
 
-  // Método para procesar el formulario de edición
-  export const actualizar = async (req, res) => {
-    try {
-      const { id, nombre } = req.body; 
+    // Generar un nuevo ID
+    const nuevoId = empresas.length + 1;
 
-      const empresasPath = path.resolve(
-        process.cwd(),
-        "src/data",
-        "empresas.json"
-      );
-      
-      const empresasData = await fs.promises.readFile(empresasPath, "utf8");
-      const empresas = JSON.parse(empresasData);
+    // Crear la nueva empresa
+    const nuevaEmpresa = {
+      id: nuevoId,
+      nombre,
+      coberturas: coberturas.map(cobertura => parseInt(cobertura)),
+    };
 
-      const empresaIndex = empresas.findIndex((e) => e.id === parseInt(id));    
+    empresas.push(nuevaEmpresa);
 
-      if (empresaIndex === -1) {
-        return res.status(404).send("Empresa no encontrada");
-      }
-          
-      let empresaExistente = false;
-      
-      empresas.forEach(empresa => {
-          if (empresa.nombre == nombre) {
-            empresaExistente == true; 
-        }
-      });
-        
+    // Guardar los cambios en el archivo JSON
+    fs.writeFileSync(empresasPath, JSON.stringify(empresas, null, 2));
 
-      if (empresaExistente) {
-        return res.status(400).send("La empersa con ese nombre ya existe");
-      }
-     
-      empresas[empresaIndex].nombre = nombre;
+    res.redirect("/empresas");
+  } catch (error) {
+    console.error("Error al crear una nueva empresa:", error.message);
+    res.status(500).send("Error al crear una nueva empresa");
+  }
+};
 
-      // Guardar los cambios en el archivo JSON
-      fs.writeFileSync(empresasPath, JSON.stringify(empresas, null, 2));
+export const detalle = async (req, res) => {
+  try {
+    const { id } = req.params; // Obtener el ID del usuario desde la URL
 
-      res.redirect("/empresas");
-    } catch (error) {
-      console.error("Error al actualizar la empresa:", error.message);
-      res.status(500).send("Error al actualizar la empresa");
+    const usuariosPath = path.resolve(
+      process.cwd(),
+      "src/data",
+      "usuarios.json"
+    );
+    const permisosPath = path.resolve(
+      process.cwd(),
+      "src/data",
+      "permisos.json"
+    );
+    const sucursalesPath = path.resolve(
+      process.cwd(),
+      "src/data",
+      "sucursales.json"
+    );
+    const usuariosData = await fs.promises.readFile(usuariosPath, "utf8");
+    const permisosData = await fs.promises.readFile(permisosPath, "utf8");
+    const sucursalesData = await fs.promises.readFile(sucursalesPath, "utf8");
+    const usuarios = JSON.parse(usuariosData);
+    const permisos = JSON.parse(permisosData);
+    const sucursales = JSON.parse(sucursalesData);
+
+    const usuario = usuarios.find((u) => u.id === parseInt(id));
+
+    if (!usuario) {
+      return res.status(404).send("Usuario no encontrado");
     }
-  };
 
+    const permiso = permisos.find((p) => p.id === usuario.permisos);
+    const sucursal = sucursales.find((s) => s.id === usuario.sucursal);
+
+    res.render("usuarios/detalle", { usuario, permiso, sucursal });
+  } catch (error) {
+    console.error("Error al cargar el detalle del usuario:", error.message);
+    res.status(500).send("Error al cargar el detalle del usuario");
+  }
+};
+
+// Método para mostrar el formulario de edición
+export const editar = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const empresasPath = path.resolve(
+      process.cwd(),
+      "src/data",
+      "empresas.json"
+    );
+
+    const empresasData = await fs.promises.readFile(empresasPath, "utf8");
+    const empresas = JSON.parse(empresasData);
+
+    const empresa = empresas.find((e) => e.id === parseInt(id));
+
+    res.render("empresas/editar", { empresa });
+  } catch (error) {
+    console.error("Error al cargar el formulario de edición:", error.message);
+    res.status(500).send("Error al cargar el formulario de edición");
+  }
+};
+
+// Método para procesar el formulario de edición
+export const actualizar = async (req, res) => {
+  try {
+    const { id, nombre } = req.body;
+
+    const empresasPath = path.resolve(
+      process.cwd(),
+      "src/data",
+      "empresas.json"
+    );
+
+    const empresasData = await fs.promises.readFile(empresasPath, "utf8");
+    const empresas = JSON.parse(empresasData);
+
+    const empresaIndex = empresas.findIndex((e) => e.id === parseInt(id));
+
+    if (empresaIndex === -1) {
+      return res.status(404).send("Empresa no encontrada");
+    }
+
+    let empresaExistente = false;
+
+    empresas.forEach((empresa) => {
+      if (empresa.nombre == nombre) {
+        empresaExistente == true;
+      }
+    });
+
+    if (empresaExistente) {
+      return res.status(400).send("La empersa con ese nombre ya existe");
+    }
+
+    empresas[empresaIndex].nombre = nombre;
+
+    // Guardar los cambios en el archivo JSON
+    fs.writeFileSync(empresasPath, JSON.stringify(empresas, null, 2));
+
+    res.redirect("/empresas");
+  } catch (error) {
+    console.error("Error al actualizar la empresa:", error.message);
+    res.status(500).send("Error al actualizar la empresa");
+  }
+};
 
 export const eliminar = (req, res) => {
-    try {
-      const { id } = req.body;
+  try {
+    const { id } = req.body;
 
-      const usuariosPath = path.resolve(process.cwd(),"src/data/usuarios.json");
-      const usuariosData = fs.readFileSync(usuariosPath, "utf8");
-      const usuarios = JSON.parse(usuariosData);
+    const usuariosPath = path.resolve(process.cwd(), "src/data/usuarios.json");
+    const usuariosData = fs.readFileSync(usuariosPath, "utf8");
+    const usuarios = JSON.parse(usuariosData);
 
-      const usuarioIndex = usuarios.findIndex((u) => u.id === parseInt(id));
+    const usuarioIndex = usuarios.findIndex((u) => u.id === parseInt(id));
 
-      if (usuarioIndex === -1) {
-        return res.status(404).send("Usuario no encontrado");
-      }
-
-      // Cambiar el estado del usuario a inactivo
-      usuarios[usuarioIndex].activo = false;
-
-      // Guardar los cambios en el archivo JSON
-      fs.writeFileSync(usuariosPath, JSON.stringify(usuarios, null, 2));
-
-      res.redirect("/usuarios");
-    } catch (error) {
-      console.error("Error al eliminar el usuario:", error.message);
-      res.status(500).send("Error al eliminar el usuario");
+    if (usuarioIndex === -1) {
+      return res.status(404).send("Usuario no encontrado");
     }
+
+    // Cambiar el estado del usuario a inactivo
+    usuarios[usuarioIndex].activo = false;
+
+    // Guardar los cambios en el archivo JSON
+    fs.writeFileSync(usuariosPath, JSON.stringify(usuarios, null, 2));
+
+    res.redirect("/usuarios");
+  } catch (error) {
+    console.error("Error al eliminar el usuario:", error.message);
+    res.status(500).send("Error al eliminar el usuario");
+  }
 };
