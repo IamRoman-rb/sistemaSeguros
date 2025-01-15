036ed8800138b157499d314c21e5dd3d172e7d33
IamRoman-rb <roman.borla2004@gmail.com>
2025-01-15 12:39:07 -0300

    Corrigiendo middleware

--- src/middlewares/loadStylesheet.js
+++ src/middlewares/loadStylesheet.js
@@ -1,6 +1,7 @@
 const loadStylesheet = (req, res, next) => {
-    const entity = req.originalUrl.split('/')[1]; // Obtiene la entidad de la URL
+    let entity = req.originalUrl.split('/')[1]; // Obtiene la entidad de la URL
     let action = req.path.split('/').filter(Boolean);
+    console.log(action);
     if(action.length > 1) action = action[1];
     res.locals.stylesheet = `/css/${entity}/${action}.css`;
     console.log("CSS",res.locals.stylesheet);
