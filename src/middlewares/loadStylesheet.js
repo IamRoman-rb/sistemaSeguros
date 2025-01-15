03961b522682d939b139441423e9e415a95dbf0d
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-01-15 12:53:30 -0300

    Arreglar el middleware style

--- src/middlewares/loadStylesheet.js
+++ src/middlewares/loadStylesheet.js
@@ -2,8 +2,21 @@ const loadStylesheet = (req, res, next) => {
     let entity = req.originalUrl.split('/')[1]; // Obtiene la entidad de la URL
     let action = req.path.split('/').filter(Boolean);
     console.log(action);
-    if(action.length > 1) action = action[1];
-    res.locals.stylesheet = `/css/${entity}/${action}.css`;
+    switch (action.length) {
+        case 1:
+           action = action[0] 
+        break;
+        
+        case 2:
+            entity = action[0];
+            action = action[1];
+        break;
+    
+        default:
+            action = action[1];
+        break;
+    }
+    res.locals.stylesheet = `${entity}/${action}.css`;
     console.log("CSS",res.locals.stylesheet);
     next();
 };
