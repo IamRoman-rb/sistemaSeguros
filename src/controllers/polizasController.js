a9eee25a276068e5db8892d10e1aa81aa774162f
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-01-21 09:45:33 -0300

    Final de polizas

--- src/controllers/polizasController.js
+++ src/controllers/polizasController.js
@@ -69,41 +69,42 @@ export const nueva = async (req, res) => {
 };
 export const guardar = async (req, res) => {
   try {
-      /* Agregar sucursal del usuario logueado */
-      const {sucursal} = req.session.user;
+    /* Agregar sucursal del usuario logueado */
+    const { sucursal } = req.session.user;
 
-      /* Validacion de patentes duplicadas */
+    /* Validacion de patentes duplicadas */
 
-      const resources = [
-          path.resolve(process.cwd(), "src/data", "polizas.json"),
-          path.resolve(process.cwd(), "src/data", "clientes.json"),
-      ]
+    const resources = [
+      path.resolve(process.cwd(), "src/data", "polizas.json"),
+      path.resolve(process.cwd(), "src/data", "clientes.json"),
+    ]
 
-      let [polizas, clientes] = await Promise.all(resources.map(async (resource) => JSON.parse(await readFile(resource, 'utf-8'))))
+    let [polizas, clientes] = await Promise.all(resources.map(async (resource) => JSON.parse(await readFile(resource, 'utf-8'))))
 
-      const polizasPatente = polizas.filter((poliza) => poliza.patente === req.body.patente);
+    const polizasPatente = polizas.filter((poliza) => poliza.patente === req.body.patente);
 
-      if (polizasPatente.length > 0) {
-          return res.status(400).send("Patente duplicada");
-      }
+    if (polizasPatente.length > 0) {
+      return res.status(400).send("Patente duplicada");
+    }
 
 
 
-      // Calcular la fecha actual
-      const fechaActual = new Date();
-      const dia = fechaActual.getDate();
-      const mes = fechaActual.getMonth() + 1; // Los meses se indican desde 0
-      const anio = fechaActual.getFullYear();
-      const fechaFormateada = `${anio}-${mes.toString().padStart(2, '0')}-${dia.toString().padStart(2, '0')}`;
-      // Calcular la fecha de vencimiento de la póliza
-      const f_vencimiento = new Date(req.body.f_ini_vigencia);
-      f_vencimiento.setMonth(f_vencimiento.getMonth() + Number(req.body.cuotas));
-      const fechaVencimientoFormateada = `${f_vencimiento.getFullYear()}-${(f_vencimiento.getMonth() + 1).toString().padStart(2, '0')}-${f_vencimiento.getDate().toString().padStart(2, '0')}`;
-      const nuevaPoliza = {
+    // Calcular la fecha actual
+    const fechaActual = new Date();
+    const dia = fechaActual.getDate();
+    const mes = fechaActual.getMonth() + 1; // Los meses se indican desde 0
+    const anio = fechaActual.getFullYear();
+    const fechaFormateada = `${anio}-${mes.toString().padStart(2, '0')}-${dia.toString().padStart(2, '0')}`;
+    // Calcular la fecha de vencimiento de la póliza
+    const f_vencimiento = new Date(req.body.f_ini_vigencia);
+    f_vencimiento.setMonth(f_vencimiento.getMonth() + Number(req.body.periodo));
+    const fechaVencimientoFormateada = `${f_vencimiento.getFullYear()}-${(f_vencimiento.getMonth() + 1).toString().padStart(2, '0')}-${f_vencimiento.getDate().toString().padStart(2, '0')}`;
+    const nuevaPoliza = {
       n_poliza: req.body.n_poliza,
       f_emision: fechaFormateada,
       f_ini_vigencia: req.body.f_ini_vigencia,
       f_fin_vigencia: fechaVencimientoFormateada,
+      periodo: Number(req.body.periodo),
       suma: Number(req.body.suma),
       cuotas: Number(req.body.cuotas),
       empresa: Number(req.body.empresa),
@@ -181,14 +182,16 @@ export const detalle = async (req, res) => {
     if (!cliente) {
       return res.status(404).send("Cliente asociado no encontrado");
     }
-    
+
     cliente.provincia = provincias.find((p) => p.id === Number(cliente.idprovincia));
     cliente.ciudad = ciudades.find((c) => c.id === Number(cliente.idciudad));
 
     poliza.empresa = empresas.find((e) => e.id === Number(poliza.empresa));
     poliza.sucursal = sucursales.find((s) => s.id === Number(poliza.sucursal));
     poliza.marca = automarcas.find((a) => a.id === Number(poliza.marca));
-    poliza.pagos = pagos.filter((pago) => pago.id_poliza === Number(id));
+    if (poliza.pagos.length > 0) {
+      poliza.pagos = pagos.filter((pago) => pago.id_poliza === Number(id));
+    }
     poliza.cobertura = coberturas.find((c) => c.id === Number(poliza.cobertura));
     // Renderizar la vista con los detalles
     res.render("polizas/detalle", { poliza, cliente });
@@ -230,7 +233,7 @@ export const confirmar = async (req, res) => {
     if (!cliente) {
       return res.status(404).send("Cliente asociado no encontrado");
     }
-    
+
     cliente.provincia = provincias.find((p) => p.id === Number(cliente.idprovincia));
     cliente.ciudad = ciudades.find((c) => c.id === Number(cliente.idciudad));
 
@@ -268,7 +271,7 @@ export const eliminar = async (req, res) => {
 
     // Buscar el cliente asociado a la póliza
     const cliente = clientes.find((c) => c.polizas.includes(Number(id)));
-    
+
     // Inhabilitar la póliza de la lista
     polizas[polizaIndex].inhabilitado = true;
 
@@ -308,11 +311,11 @@ export const editar = async function editarPoliza(req, res) {
 
     // Encontrar la póliza a editar
     const poliza = polizas.find(poliza => poliza.id === parseInt(id));
-    
+
     if (!poliza) {
       return res.status(404).send('Póliza no encontrada');
     }
-  
+
     // Encontrar el cliente
     const cliente = clientes.find(cliente => cliente.id === parseInt(poliza.clienteId));
 
@@ -353,22 +356,26 @@ export const actualizar = async (req, res) => {
         cambios[campo] = req.body[campo];
       }
     }
-    if(campos.includes("empresa") && req.body.empresa !== polizas[index].empresa) {
+    if (campos.includes("empresa") && req.body.empresa !== polizas[index].empresa) {
       cambios.empresa = Number(req.body.empresa);
     }
-    if(campos.includes("marca") && req.body.marca !== polizas[index].marca) {
+    if (campos.includes("marca") && req.body.marca !== polizas[index].marca) {
       cambios.marca = Number(req.body.marca);
     }
-    if(campos.includes("cobertura") && req.body.cobertura !== polizas[index].cobertura) {
+    if (campos.includes("cobertura") && req.body.cobertura !== polizas[index].cobertura) {
       cambios.cobertura = Number(req.body.cobertura);
     }
-    if(campos.includes("cuotas") && req.body.cuotas !== polizas[index].cuotas) {
+    if (campos.includes("cuotas") && req.body.cuotas !== polizas[index].cuotas) {
       cambios.cuotas = Number(req.body.cuotas);
     }
 
-    if(campos.includes("cuotas") && cambios.cuotas != polizas[index].cuotas) {
+    if (campos.includes("periodo") && req.body.periodo !== polizas[index].periodo) {
+      cambios.periodo = Number(req.body.periodo);
+    }
+
+    if (campos.includes("periodo") && cambios.periodo != polizas[index].periodo) {
       const f_vencimiento = new Date(polizas[index].f_ini_vigencia);
-      f_vencimiento.setMonth(f_vencimiento.getMonth() + Number(cambios.cuotas));
+      f_vencimiento.setMonth(f_vencimiento.getMonth() + Number(cambios.periodo));
       cambios.f_fin_vigencia = `${f_vencimiento.getFullYear()}-${(f_vencimiento.getMonth() + 1).toString().padStart(2, '0')}-${f_vencimiento.getDate().toString().padStart(2, '0')}`;
     }
 
