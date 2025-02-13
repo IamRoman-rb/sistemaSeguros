9f71b57106b1cdb825c1708fb579da9ec925d602
IamRoman-rb <roman.borla2004@gmail.com>
2025-02-12 22:00:33 -0300

    Formulario para editar automarca

--- src/controllers/auxiliearesController.js
+++ src/controllers/auxiliearesController.js
@@ -44,3 +44,59 @@ export const auxiliares = async (req, res) => {
         return res.status(500).send('Error al listar los datos'); // No enviar el error directamente al cliente en producción
     }
 }
+
+export const editar = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        const resources = [
+            path.resolve(process.cwd(), "src/data", "automarcas.json"),
+        ];
+        let [automarcas] = await Promise.all(
+            resources.map(async (resource) => JSON.parse(await readFile(resource, 'utf-8')))
+        );
+
+        let marca = automarcas.find( (m) => m.id == Number(id) );
+        
+        res.status(200).render('auxiliares/editar', { marca });
+
+    } catch (error) {
+        res.status(500).send('Error', error);
+        console.error(error);
+        
+    }
+}
+export const actualizar = async (req, res) => {
+    try {
+        const { id, nombre } = req.body; // Get id from params now
+
+        const resources = [
+            path.resolve(process.cwd(), "src/data", "automarcas.json"),
+        ];
+        let [automarcas] = await Promise.all(
+            resources.map(async (resource) => JSON.parse(await readFile(resource, 'utf-8')))
+        );
+
+        let marcaIndex = automarcas.findIndex( (m) => m.id == id );
+
+        
+
+        if (marcaIndex !== -1) { // Check if the marca was found
+            automarcas[marcaIndex].marca = nombre;
+
+            await Promise.all([
+                writeFile(resources[0], JSON.stringify(automarcas, null, 2)),
+            ]);
+
+            res.redirect('/auxiliares'); // Redirect to the correct path
+        } else {
+            res.status(404).send("Marca no encontrada"); // Handle the case where the marca is not found
+        }
+
+
+    } catch (error) {
+        res.status(500).send('Error', error);
+        console.error(error);        
+    }
+}
--- src/routes/auxilieares.js
+++ src/routes/auxilieares.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import { auxiliares } from "../controllers/auxiliearesController.js"; // Usamos 'import' en lugar de 'require'
+import { auxiliares, editar, actualizar } from "../controllers/auxiliearesController.js"; // Usamos 'import' en lugar de 'require'
 import hasPermitions from "../middlewares/hasPermitions.js";
 const router = Router();
 
 router.get("/", hasPermitions, auxiliares);
 // router.get("/nuevo", hasPermitions, nuevo);
 // router.get('/confirmar/:id', hasPermitions, confirmar);
-// router.get('/editar/:id', hasPermitions, editar);
+router.get('/editar/:id', hasPermitions, editar);
 // router.post('/guardar', guardar);
-// router.post('/actualizar', actualizar);
+router.post('/actualizar', actualizar);
 // router.post('/eliminar', eliminar);
 
 export default router;
